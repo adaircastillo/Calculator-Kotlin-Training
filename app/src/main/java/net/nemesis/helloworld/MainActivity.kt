0eeff6d8b7e21dcf018701d5b68039a6a17cd0fd
Adair Castillo <adair.castillo@dexcom.com>
2022-06-16 11:35:06 -0500

    Bindings

--- app/src/main/java/net/nemesis/helloworld/MainActivity.kt
+++ app/src/main/java/net/nemesis/helloworld/MainActivity.kt
@@ -3,9 +3,11 @@ package net.nemesis.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import net.nemesis.helloworld.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,10 +15,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupBindings()
+    }
 
-        binding.keyClear.setOnClickListener {
-            Log.e("TEST", "Click en boton Clear")
+    private fun setupBindings(){
+
+        with(binding){
+
+            keyClear.setOnClickListener {
+
+                Log.e("TEST", "Click en boton Clear")
+            }
+
+            key0.setOnClickListener(this@MainActivity)
+            key1.setOnClickListener(this@MainActivity)
+            key2.setOnClickListener(this@MainActivity)
+            key3.setOnClickListener(this@MainActivity)
+            key4.setOnClickListener(this@MainActivity)
+            key5.setOnClickListener(this@MainActivity)
+            key6.setOnClickListener(this@MainActivity)
+            key7.setOnClickListener(this@MainActivity)
+            key8.setOnClickListener(this@MainActivity)
+            key9.setOnClickListener(this@MainActivity)
         }
+    }
+
+    override fun onClick(v: View?) {
+
+        if(v is Button){
+
+            val number = v.text.toString()
+
+            Log.e("TEST", "Click en boton $number")
+
+        }
+
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
